218f6c6389707fd4b8b547a05f33843f42d9001f
JaxPelzer <er7321re@go.minnstate.edu>
2022-05-01 14:34:59 -0500

    unitTest

---
+++ Group2_CompRepair/Group2_CompRepairTests/Controllers/ComputerPartsControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group2_CompRepair.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group2_CompRepair.Controllers;
+using Group2_CompRepair.Data;
+using Group2_CompRepair.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group2_CompRepair.Controllers.Tests
+{
+    [TestClass()]
+    public class ComputerPartsControllerTests
+    {
+        private readonly Group2_ComprepairContext _context;
+        [TestMethod()]
+        public void GetComputerPartTest()
+        {
+            int id = 20;
+            var controller = new ComputerPartsController(_context);
+
+            
+            Assert.IsInstanceOfType(controller.GetComputerPart(id), typeof(Task<ActionResult<ComputerPart>>));
+            
+        }
+
+        [TestMethod()]
+        public void GetComputerPartsTest()
+        {
+            
+            var controller = new ComputerPartsController(_context);
+            
+
+
+            Assert.IsNotNull(controller.GetComputerParts());
+        }
+
+        [TestMethod()]
+        public void GetComputerPartsTypeTest()
+        {
+
+            var controller = new ComputerPartsController(_context);
+
+
+
+            Assert.IsInstanceOfType(controller.GetComputerParts(), typeof(Task<ActionResult<IEnumerable<ComputerPart>>>));
+        }
+
+
+    }
+} 
---
+++ Group2_CompRepair/Group2_CompRepairTests/JwtAuthenticationManagerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group2_CompRepair;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group2_CompRepair.Controllers;
+
+namespace Group2_CompRepair.Tests
+{
+    [TestClass()]
+    public class JwtAuthenticationManagerTests
+    {
+        [TestMethod()]
+        public void AuthenticateTestNull()
+        {
+
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("fakekeynotlogin1111!");
+
+            User user = new User
+            {
+                username = "testUser",
+                password = "testPassword"
+            };
+
+            var ret = manager.Authenticate(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod()]
+        public void AuthenticateTestNotNull()
+        {
+
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("fakekeynotlogin1111!");
+
+            User user = new User
+            {
+                username = "test",
+                password = "password"
+            };
+
+            var ret = manager.Authenticate(user.username, user.password);
+
+            Assert.IsNotNull(ret);
+        }
+    }
+}
